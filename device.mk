429ca32b3f28cd4ed9856a40449571a4525c2ea2
rik0612c <qq872638399@gmail.com>
2023-05-27 11:56:44 +0800

    addison: Uprev audio hal to 6.0

--- device.mk
+++ device.mk
@@ -27,9 +27,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
